d64ce45ce61e04509ab35241b49afce07b84d90e
Christine Nguyen <cnhnguyen59@gmail.com>
2021-12-09 22:46:14 -0500

    wrote function to call api and added weather icons'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
 var inputEl = $('input')
+var currCityEl = $('.currCity')
 var city = inputEl.val()
-var currCityEl= $('.currCity')
 const apiKey =`7b9f6bec4c139049a453497279489bbe`
-
-/* var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}exclude=${exclude}&&appid=${apiKey}` */
-
-
-function getApi() {
-
-    currCityEl.text(`${city} ${moment().format('L')}`)
+var lat = localStorage.getItem('lat')
+var lon = localStorage.getItem('lon')
+const part ='hourly'
 
 
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputEl.val()}&appid=${apiKey}`
+function getInfo(){
+   /*  var city = inputEl.val()
+    console.log(city)
 
-    /* `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}exclude=${exclude}&&appid=${apiKey}` */
-    
-    fetch(requestUrl)
+    currCityEl.text(`${city} ${moment().format('L')}`) */
+
+    getWeather()
+
+    lat = localStorage.getItem('lat')
+    lon = localStorage.getItem('lon')
+
+    var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&units=imperial&appid=${apiKey}`
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputEl.val()}&exclude=hourly&appid=${apiKey}`
+
+    console.log(oneCallUrl)
+  
+    fetch(forecastUrl)
     .then(function(response){
         return response.json();
     })
@@ -24,8 +32,73 @@ function getApi() {
     })
     .catch(console.err)
 
+    fetch(oneCallUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        console.log(data.current.temp)
+        console.log(data.current.wind_speed)
+        console.log(data.current.humidity)
+        console.log(data.current.uvi)
+
+        var temp = data.current.temp
+        var ws = data.current.wind_speed
+        var humid = data.current.humidity
+        var uvi = data.current.uvi
+        var color =''
+
+        if (uvi < .5){
+            color = 'green'
+        } else if(uvi < .8 && uvi >.5){
+            color = 'yellow'
+        } else {
+            color = 'red'
+        }
+
+        var output = `Temp: ${temp} | Wind: ${ws} | Humidity: ${humid} | UV Index: <span class="py-1 px-3" style="color: white; border-radius: 1px; background-color:${color}">${uvi}</span>`
+
+        $('.currWeather').html(output)
+
+    })
+  
+
 }
 
+function getWeather(){
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputEl.val()}&units=imperial&appid=${apiKey}`
+ 
+     
 
+    fetch(weatherUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        console.log(data.coord)
+        localStorage.setItem('lat', JSON.stringify(data.coord.lat))
+        localStorage.setItem('lon', JSON.stringify(data.coord.lon))
+        console.log(data.coord.lat)
+        console.log(data.coord.lon)
+        localStorage.getItem('lat')
+        localStorage.getItem('lon')
+        console.log(data.weather.icon)
 
-    
+        var city = data.name
+        var icon = data.weather[0].icon
+
+        $('.currIcon').html(`<img src="./assets/icons/${icon}.png"/>`)
+        
+
+        currCityEl.text(`Currently in ${city}` /* ${moment().format('L')} */)
+
+    })
+    .catch(console.err)
+
+}
+
+function getForecast(){}
+
+function createHistBtn(){}
